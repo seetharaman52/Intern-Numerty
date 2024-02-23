8fb5608fdc0f1940c2b4a011dbd11385c3fbff4a
seetharaman52 <seetharamanshan699@gmail.com>
2024-02-23 11:22:15 +0530

    Deleted a folder

--- Day 6 -Restful API/myapp/HarvestHub/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from HarvestHub.models import Myapp
-
-class MyappSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Myapp
-        fields = '__all__'
